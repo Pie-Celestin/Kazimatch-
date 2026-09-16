effad2a22f148e4cc9938eebccc4ea0cfa202d14
Pie-Celestin <habaruremapierrecelestin4@gmail.com>
2026-09-16 08:09:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,9 @@
-const SUPABASE_URL = https://zfzlocesnzhdcdhbbtht.supabase.co/rest/v1/
-const SUPABASE_KEY = sb_publishable_AfXQlQ1dY4VpiVTGDE_j0RA_1c2T9xni
+// ==========================================
+// KAZIMATCH SUPABASE CONFIGURATION
+// ==========================================
+
+window.SUPABASE_URL =
+  https://zfzlocesnzhdcdhbbtht.supabase.co/rest/v1/;
+
+window.SUPABASE_KEY =
+  sb_publishable_AfXQlQ1dY4VpiVTGDEj0RA_1c2T9xni;
